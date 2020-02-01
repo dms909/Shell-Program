5e8db3b7ed2e5b11e6d2a91e622d68f005e610a0
Dylan Smith <dylan1999.smith@gmail.com>
2020-01-31 20:08:25 -0600

    First commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define MAX_LINE 80
+
+int main(void){
+	char *args[MAX_LINE/2 + 1];
+
+	int should_run = 1;
+
+	while(should_run){
+		printf("osh");
+		fflush(stdout);
+	}
+
+	return 0;
+}
