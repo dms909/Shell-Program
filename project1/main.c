543791490457e63d18cf88e028f4adfc2ba51680
Dylan Smith <dylan1999.smith@gmail.com>
2020-01-31 21:16:36 -0600

    Figuring stuff out - check comments

--- project1/main.c
+++ project1/main.c
@@ -4,16 +4,17 @@
 #define MAX_LINE 80
 
 int main(void){
-	char *args[MAX_LINE/2 + 1];
+	char *args[MAX_LINE/2 + 1]; // this is a string array - this is where we will store every user command 
 
 	int should_run = 1;
 	
-	fgets(*args, MAX_LINE, stdout);	
-	while(should_run){
-	//	printf("osh>");
-	//	fgets(*args, MAX_LINE, stdout);	
+	fgets(args, MAX_LINE, stdin);
+	printf("Input was: %s\n", args);	
+	/*while(should_run){
+		printf("osh>");
+		fgets(*args, MAX_LINE, stdout);	
 		fflush(stdout);
-	}
+	}*/
 
 	return 0;
 }
