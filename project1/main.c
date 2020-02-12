60fb43b76c35634cfb8c52839b662b8d508569ec
Dylan Smith <dylan1999.smith@gmail.com>
2020-02-11 22:31:51 -0600

    Figured stuff out, should be on the right track

--- project1/main.c
+++ project1/main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
 #define MAX_LINE 80
 
 int main(void){
 	char *args[MAX_LINE/2 + 1]; // this is a string array - this is where we will store every user command 
-
+	char user_input[MAX_LINE];
 	int should_run = 1;
 	
-	fgets(args, MAX_LINE, stdin);
-	printf("Input was: %s\n", args);	
-	/*while(should_run){
+	while(should_run){
 		printf("osh>");
-		fgets(*args, MAX_LINE, stdout);	
 		fflush(stdout);
-	}*/
+
+		scanf("%s", user_input);
+
+		if(!strcmp(user_input, "exit"))  return 0;
+
+		*args = strtok(user_input, " ");
+		printf("%s", args[1]);
+		/*pid_t pid = fork();  
+		if(!pid){
+			printf("This is the child process");
+		} else{
+			printf("This is the parent process");
+		}
+*/
+		printf("\n");
+	}
 
 	return 0;
 }
