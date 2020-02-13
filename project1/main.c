b558bbe301df3154873f29b651ed8d4fa1accf6c
Dylan Smith <dylan1999.smith@gmail.com>
2020-02-13 12:50:17 -0600

    got execvp working... onto pipelining

--- project1/main.c
+++ project1/main.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_LINE 80
 
 int main(void){
 	char *args[MAX_LINE/2 + 1]; // this is a string array - this is where we will store every user command 
 	char user_input[MAX_LINE];
+	char *history;
 	int should_run = 1;
 	
 	while(should_run){
-		printf("osh>");
+		printf("osh> ");
 		fflush(stdout);
-
-		scanf("%s", user_input);
-
+		scanf ("%[^\n]%*c", user_input);
+	
 		if(!strcmp(user_input, "exit"))  return 0;
+		
+		args[0] = strtok(user_input, " ");
 
-		*args = strtok(user_input, " ");
-		printf("%s", args[1]);
-		/*pid_t pid = fork();  
-		if(!pid){
-			printf("This is the child process");
-		} else{
-			printf("This is the parent process");
+		int i = 0;
+		while(args[i] != NULL){
+			i++;
+			args[i] = strtok(NULL, " ");
 		}
-*/
-		printf("\n");
+		int j;
+				
+		pid_t pid = fork();
+		if(pid < 0){
+			printf("Error on child process creation, exiting...");
+			return -1;
+		}  
+		
+		if(pid == 0){
+			execvp(args[0], args);
+			printf("Command not found\n");
+			exit(1);	
+		} else{
+			waitpid(pid, NULL, 0);	
+			int j;
+			history = malloc(sizeof user_input);
+			for(j=0; j<i; j++){
+				args[j] = NULL;
+			}
+		}	
 	}
 
 	return 0;
