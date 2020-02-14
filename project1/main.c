abf56c06e983ad7867d0645a154c7184edc9d459
Dylan Smith <dylan1999.smith@gmail.com>
2020-02-13 18:47:18 -0600

    hope i dont mess anything up lolllllllllllllllllllllllll

--- project1/main.c
+++ project1/main.c
@@ -11,16 +11,32 @@
 int main(void){
 	char *args[MAX_LINE/2 + 1]; // this is a string array - this is where we will store every user command 
 	char user_input[MAX_LINE];
-	char *history;
+	char history[MAX_LINE];
 	int should_run = 1;
+	int input;
+	int output;
 	
+	strcpy(history, "");
+	input = 0;
+	output = 0;
+
 	while(should_run){
 		printf("osh> ");
 		fflush(stdout);
-		scanf ("%[^\n]%*c", user_input);
+		scanf("%[^\n]%*c", user_input);
 	
 		if(!strcmp(user_input, "exit"))  return 0;
-		
+	
+		if(!strcmp(user_input, "!!")){
+			if(!strcmp(history, "")){
+				printf("No commands in history\n");
+			}
+			else{
+				strcpy(user_input, history);
+				printf("osh> %s\n", user_input);
+			}
+		}
+
 		args[0] = strtok(user_input, " ");
 
 		int i = 0;
@@ -37,13 +53,18 @@ int main(void){
 		}  
 		
 		if(pid == 0){
-			execvp(args[0], args);
-			printf("Command not found\n");
-			exit(1);	
+			if(strcmp(user_input, "") && strcmp(user_input, "!!")){
+				execvp(args[0], args);
+				printf("Command not found\n");
+				exit(1);
+			}
+			exit(1);
 		} else{
 			waitpid(pid, NULL, 0);	
 			int j;
-			history = malloc(sizeof user_input);
+			if(strcmp(user_input, "!!")){
+				strcpy(history, user_input);
+			}
 			for(j=0; j<i; j++){
 				args[j] = NULL;
 			}
